ad957d390444e9f09ae075bc3fb4980b3f9e0069
tanmayhinge <hingetanmay@gmail.com>
2021-04-06 13:57:37 +0530

    inlining

--- src/HomePage/Footer.js
+++ src/HomePage/Footer.js
@@ -3,43 +3,42 @@ import IconButton from '@material-ui/core/IconButton';
 import AndroidIcon from '@material-ui/icons/Android';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import Tooltip from '@material-ui/core/Tooltip';
-import './Footer.css';
-import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
-    title: {
-      flexGrow: 1,
-      textAlign: 'left',
-      verticalAlign: 'bottom',
-    },
-    small: {
-      width: theme.spacing(4),
-      height: theme.spacing(4),
-    },
-    sectionDesktop: {
-      display: 'none',
-      [theme.breakpoints.up('sm')]: {
-        display: 'flex',
-      },
-    },
-    sectionMobile: {
-      display: 'flex',
-      [theme.breakpoints.up('sm')]: {
-        display: 'none',
-      },
-    },
-  }));
-
-export default ({ className, children }) => {
-    const classes = useStyles();
+export default () => {
     return(
-        <>
-        <a href="https://github.com/" rel="noopener noreferrer" target="_blank" style={{ textDecoration: 'none' }}>
-          <GitHubIcon className="fa" color="action" fontSize="small" />
-        </a>
-        <a href="https://github.com/" rel="noopener noreferrer" target="_blank" style={{ textDecoration: 'none' }}>
-          <AndroidIcon className="fa" color="action" fontSize="small" />
-        </a>
-      </>
+        <div>
+        <Tooltip title="GitHub"  style={{margin:"10px"}}>
+                <IconButton
+                  aria-label="android app"
+                  aria-controls="menu-appbar"
+                  color="inherit"
+                >
+                  <a
+                    href="https://github.com/"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    style={{ color: 'inherit', textDecoration: 'none', height: 24 }}
+                  >
+                    <GitHubIcon />
+                  </a>
+                </IconButton>
+        </Tooltip>
+        <Tooltip title="Android App"  style={{margin:"10px"}}>
+                <IconButton
+                  aria-label="android app"
+                  aria-controls="menu-appbar"
+                  color="inherit"
+                >
+                  <a
+                    href="https://github.com/"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    style={{ color: 'inherit', textDecoration: 'none', height: 24 }}
+                  >
+                    <AndroidIcon />
+                  </a>
+                </IconButton>
+        </Tooltip>
+      </div>
     )   
 };
